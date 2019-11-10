563814f65c488ed005f462f3b01b1726c6d73a7b
ahmedElghandour1 <elghandour.12@gmail.com>
2019-11-10 23:59:06 +0200

    express middleware

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,28 +1,72 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
+
 const app = express();
+
 app.set('view engine', 'hbs');
-app.use(express.static(`${__dirname}/public`))
-app.get('/', (req, res) => {
+hbs.registerPartials(`${__dirname}/views/partials`);
+app.use((req, res, next) => {
+    const baseUrl = req.baseUrl;
+    const connection = req.connection;
+    const cookies = req.cookies;
+    const host = req.hostname;
+    const ip = req.ip;
+    const url = req.url;
+    let logArr = {
+        baseUrl,
+        cookies,
+        host,
+        ip,
+        url,
+    };
+    fs.appendFileSync('server.log', JSON.stringify(logArr, 4) + '\n')
+    next();
+})
+// app.use((req, resp, next) => {
+//     resp.render('maintenance.hbs')
+// })
+
+app.use(express.static(`${__dirname}/public`));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('toUpperCase', (txt) => {
+    return txt.toUpperCase();
+})
+
+app.get('/json', (req, res) => {
     // res.send('<h1>Hello Express!</h1>');
     res.send({
         name: 'ahmed',
         likes: [2321, 2321, 3214124]
-    })
-})
+    });
+});
+
+app.get('/', (req, res) => {
+    // res.send('About page')
+    res.render('home.hbs', {
+        title: 'Home Page',
+        msg: 'Welcome to our new website',
+        currentYear: new Date().getFullYear()
+    });
+});
+
 app.get('/about', (req, res) => {
     // res.send('About page')
     res.render('about.hbs', {
         title: 'About Page',
         currentYear: new Date().getFullYear()
-    })
-})
+    });
+});
 
 app.get('/bad', (req, res) => {
     res.send({
         errorMesssage: 'Unable to handle request'
-    })
-})
+    });
+});
+
 app.listen(3000, () => {
     console.log('server is up')
 });
